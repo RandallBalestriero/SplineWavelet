ed79efe9240993faf107f950d0f6aa961a0521c8
RandallBalestriero <randallbalestriero@gmail.com>
2018-01-29 14:20:02 -0600

    test

--- utils_old.py
+++ utils_old.py
@@ -41,25 +41,25 @@ class theano_hermite_complex:
                         thetas_imag = roll(aa,1)*hanning(S)**2
                         gammas_real = zeros(S)
                         gammas_imag = zeros(S)
-                        c           = zeros(1)-1
+                        c           = zeros(1)
                 elif(initialization=='random'):
-                        thetas_real = randn(S)
-                        thetas_imag = randn(S)
-                        gammas_real = randn(S)
-                        gammas_imag = randn(S)
+                        thetas_real = rand(S)*2-1
+                        thetas_imag = rand(S)*2-1
+                        gammas_real = rand(S)*2-1
+                        gammas_imag = rand(S)*2-1
                         if(chirplet):
-                                c   = randn(1)-1
+                                c   = zeros(1)
                         else:
-                                c   = zeros(1)-1
+                                c   = zeros(1)
                 elif(initialization=='random_apodized'):
-                        thetas_real = randn(S)*hanning(S)**2
-                        thetas_imag = randn(S)*hanning(S)**2
-                        gammas_real = randn(S)*hanning(S)**2
-                        gammas_imag = randn(S)*hanning(S)**2
+                        thetas_real = (rand(S)*2-1)*hanning(S)**2
+                        thetas_imag = (rand(S)*2-1)*hanning(S)**2
+                        gammas_real = (rand(S)*2-1)*hanning(S)**2
+                        gammas_imag = (rand(S)*2-1)*hanning(S)**2
                         if(chirplet):
-                                c   = randn(1)-1
+                                c   = zeros(1)
                         else:
-                                c   = zeros(1)-1
+                                c   = zeros(1)
 		if(chirplet):
                 	self.c            = theano.shared(c.astype('float32'))
                 self.thetas_real  = theano.shared(thetas_real.astype('float32'))
@@ -69,17 +69,22 @@ class theano_hermite_complex:
 		# NOW CREATE THE POST PROCESSED VARIABLES
 		self.ti           = th_linspace(0,1,self.S).dimshuffle([0,'x'])# THIS REPRESENTS THE MESH
                 if(self.chirplet):
-                        TT          = 3*theano.tensor.nnet.sigmoid(self.c).repeat(S)*float32(2*3.14159)*th_linspace(0,1,S)**2
-			TTc         = 3*theano.tensor.nnet.sigmoid(self.c).repeat(S)*float32(2*3.14159)*th_linspace(0,1,S)
-                        thetas_real = self.thetas_real*theano.tensor.cos(TT)-self.thetas_imag*theano.tensor.sin(TT)
-                        thetas_imag = self.thetas_imag*theano.tensor.cos(TT)+self.thetas_real*theano.tensor.sin(TT)
-			gammas_real = self.gammas_real*theano.tensor.cos(TT)-self.gammas_imag*theano.tensor.sin(TT)-self.thetas_real*theano.tensor.sin(TT)*TTc-self.thetas_imag*theano.tensor.cos(TT)*TTc
-                        gammas_imag = self.gammas_imag*theano.tensor.cos(TT)+self.gammas_real*theano.tensor.sin(TT)+self.thetas_real*theano.tensor.cos(TT)*TTc-self.thetas_imag*theano.tensor.sin(TT)*TTc
+			thetas_real = self.thetas_real
+                        thetas_imag = self.thetas_imag
+                        gammas_real = self.gammas_real
+                        gammas_imag = self.gammas_imag
+			c = theano.tensor.clip(self.c,0,1)
+                        TT          = c.repeat(S)*float32(2*3.14159)*(th_linspace(0,1,S)**2)
+			TTc         = c.repeat(S)*float32(2*3.14159)*th_linspace(0,1,S)
+                        thetas_real = thetas_real*theano.tensor.cos(TT)-thetas_imag*theano.tensor.sin(TT)
+                        thetas_imag = thetas_imag*theano.tensor.cos(TT)+thetas_real*theano.tensor.sin(TT)
+			gammas_real = gammas_real*theano.tensor.cos(TT)-gammas_imag*theano.tensor.sin(TT)-thetas_real*theano.tensor.sin(TT)*TTc-thetas_imag*theano.tensor.cos(TT)*TTc
+                        gammas_imag = gammas_imag*theano.tensor.cos(TT)+gammas_real*theano.tensor.sin(TT)+thetas_real*theano.tensor.cos(TT)*TTc-thetas_imag*theano.tensor.sin(TT)*TTc
 		else:
 			thetas_real = self.thetas_real
 			thetas_imag = self.thetas_imag
 			gammas_real = self.gammas_real
-			gamma_imag  = self.gammas_imag
+			gammas_imag  = self.gammas_imag
 		#NOW APPLY BOUNDARY CONDITION
                 self.pthetas_real   = ((thetas_real-thetas_real[1:-1].mean())*self.mask).dimshuffle([0,'x'])
                 self.pthetas_imag   = ((thetas_imag-thetas_imag[1:-1].mean())*self.mask).dimshuffle([0,'x'])
@@ -92,9 +97,9 @@ class theano_hermite_complex:
 		real_filter     = self.interp((t-self.ti[:-1])/(self.ti[1:]-self.ti[:-1]),self.pthetas_real[:-1],self.pgammas_real[:-1],self.pthetas_real[1:],self.pgammas_real[1:]).sum(0)
                 imag_filter     = self.interp((t-self.ti[:-1])/(self.ti[1:]-self.ti[:-1]),self.pthetas_imag[:-1],self.pgammas_imag[:-1],self.pthetas_imag[1:],self.pgammas_imag[1:]).sum(0)
 		# RENORMALIZE
-#		K = self.renormalization(real_filter)+self.renormalization(imag_filter)
-#                real_filter     = real_filter/(K+0.00001)
-#                imag_filter     = imag_filter/(K+0.00001)
+		K = self.renormalization(real_filter)+self.renormalization(imag_filter)
+                real_filter     = real_filter/(K+0.00001)
+                imag_filter     = imag_filter/(K+0.00001)
                 return real_filter,imag_filter
         def interp(self,t,pi,mi,pip,mip):
                 values = ((2*t**3-3*t**2+1)*pi+(t**3-2*t**2+t)*mi+(-2*t**3+3*t**2)*pip+(t**3-t**2)*mip)
@@ -103,24 +108,31 @@ class theano_hermite_complex:
 
 
 
-def create_center_filter_complex(row,filter_length,last_fb,max_length,class_function):
+def create_center_filter_complex(filter_length,max_length,class_function):
 	deltaT = max_length-filter_length
+	print deltaT
         real_f,imag_f =class_function(filter_length)
-        last_fb = theano.tensor.inc_subtensor(last_fb[2*row,deltaT/2:deltaT/2+filter_length],real_f)
-        last_fb = theano.tensor.inc_subtensor(last_fb[2*row+1,deltaT/2:deltaT/2+filter_length],imag_f)
-        return last_fb
+	filters = theano.tensor.concatenate([real_f.reshape((1,-1)),imag_f.reshape((1,-1))],axis=0)
+#	if(deltaT!=0):
+	return theano.tensor.roll(theano.tensor.concatenate([filters,theano.tensor.zeros((2,deltaT),dtype='float32')],axis=1),deltaT/2,axis=1)
+#	else:
+#		return filters
+#        last_fb = theano.tensor.inc_subtensor(last_fb[2*row,deltaT/2:deltaT/2+filter_length],real_f)
+#        last_fb = theano.tensor.inc_subtensor(last_fb[2*row+1,deltaT/2:deltaT/2+filter_length],imag_f)
+#        return last_fb
 
 
 
 def create_filter_banks_complex(filter_class,T,J,Q):
         scales = array(2**(arange(J*Q+1,dtype='float32')/Q)).astype('float32')#all the scales
         Ts     = array([T*scale for scale in scales]).astype('int32')#all the filter size support
-	Ts_t   = theano.shared(Ts)
+#	Ts_t   = theano.shared(Ts)
 	print "Lengths of the filters:",Ts
 #	filters = [filter_class.get_filters(t) for t in Ts]
-        filter_bank,_=theano.scan(fn = lambda row,filter_length,last_fb,max_length: create_center_filter_complex(row,filter_length,last_fb,max_length,filter_class.get_filters),sequences=[theano.tensor.arange(J*Q+1,dtype='int32'),Ts_t],non_sequences=[Ts_t[-1]],n_steps=J*Q+1,outputs_info=theano.tensor.zeros(((J*Q+1)*2,Ts[-1]),dtype='float32'))
-	filter_bank=filter_bank[-1]
-        return filter_bank[::2],filter_bank[1::2],Ts[-1]
+	filter_bank = theano.tensor.concatenate([create_center_filter_complex(i,Ts[-1],filter_class.get_filters) for i in Ts[:-1]],axis=0)
+#        filter_bank,_=theano.scan(fn = lambda filter_length,max_length: create_center_filter_complex(filter_length,max_length,filter_class.get_filters),sequences=Ts[:-1],non_sequences=Ts[-1])
+#	filter_bank=filter_bank[-1]
+        return filter_bank[::2,:],filter_bank[1::2,:],Ts[-1]#filter_bank[::2],filter_bank[1::2],Ts[-1]
 
 
 def create_center_filter_real(row,filter_length,last_fb,max_length,class_function):
@@ -165,11 +177,11 @@ class SplineFilter1D(lasagne.layers.Layer):
 		if(complex_):
 	                self.filter_class  = theano_hermite_complex(S,deterministic=deterministic,renormalization=renormalization,initialization=initialization,chirplet=chirplet)
        		        real_filter_bank_,imag_filter_bank_,T= create_filter_banks_complex(self.filter_class,N,J,Q)
-       		        self.real_filter_bank      = theano.tensor.cast(real_filter_bank_.reshape((J*Q+1,1,T)),'float32')
-       		        self.imag_filter_bank      = theano.tensor.cast(imag_filter_bank_.reshape((J*Q+1,1,T)),'float32')
-			self.get_filters           = theano.function([],[self.real_filter_bank,self.real_filter_bank])
-       	       		self.real_layer            = lasagne.layers.Conv1DLayer(self.input_shape,num_filters=int(J*Q+1),filter_size=int(T),W=self.real_filter_bank,stride=int(stride),pad=pad,nonlinearity=None,b=None)
-               		self.imag_layer            = lasagne.layers.Conv1DLayer(self.input_shape,num_filters=int(J*Q+1),filter_size=int(T),W=self.imag_filter_bank,stride=int(stride),pad=pad,nonlinearity=None,b=None)
+       		        self.real_filter_bank      = theano.tensor.cast(real_filter_bank_.reshape((J*Q,1,T)),'float32')
+       		        self.imag_filter_bank      = theano.tensor.cast(imag_filter_bank_.reshape((J*Q,1,T)),'float32')
+			self.get_filters           = theano.function([],[self.real_filter_bank,self.imag_filter_bank])
+       	       		self.real_layer            = lasagne.layers.Conv1DLayer(self.input_shape,num_filters=int(J*Q),filter_size=int(T),W=self.real_filter_bank,stride=int(stride),pad=pad,nonlinearity=None,b=None)
+               		self.imag_layer            = lasagne.layers.Conv1DLayer(self.input_shape,num_filters=int(J*Q),filter_size=int(T),W=self.imag_filter_bank,stride=int(stride),pad=pad,nonlinearity=None,b=None)
 		#	filters,Ts = create_filter_banks_complex(self.filter_class,N,J,Q)
 		#	self.Ts=Ts
 		#	self.filters_real = [f[0] for f in filters]
